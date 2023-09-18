3284664a6b7a1a990402a37317c950dd9c9e841e
lahirumenik <e19236@eng.pdn.ac.lk>
2023-09-18 11:31:29 +0530

    add of paatern detection in c++

---
+++ patterAlgo/pattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <string>
+
+using namespace std;
+map<int, int> mapdecoder; 
+
+void addNewCharacterToMap(int code, char c){
+    mapdecoder[code] = c-'a';
+
+}
+int encodeButtons(int one, int two, int three, int four, int five, int six){
+
+}
+
+int main()
+{
+
+    cout << "hello" << endl;
+    return 0;
+}
+   
