a694e7dba3933597acccb3bd7e588fcf5dc7f5be
lahirumenik <e19236@eng.pdn.ac.lk>
2023-09-18 22:10:57 +0530

    add space button pattern along with speech

---
+++ keyPattern/pattern.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <string>
+#include <cmath>
+#include <array>
+
+using namespace std;
+map<int, int> mapdecoder; 
+
+void addNewCharacterToMap(int code, char c){
+    mapdecoder[code] = c-'a';
+
+}
+int encodeButtons(int arr[], int size){
+    int code = 0;
+    for (int i=0; i<size; i++){
+        code += pow(2, i)*arr[i];
+
+    }
+    return code;
+}
+void addNewCharacter(int arr[], char c){
+    int code = encodeButtons(arr, 6);
+    addNewCharacterToMap(code, c);
+}
+char getCharacter(int code){
+    char c = mapdecoder[code] + 'a';
+    return c;
+}
+
+map<char, array<int, 6>> keyPatterns;
+keyPatterns['a'] = {};
+int main()
+{
+
+    cout << "hello" << endl;
+    return 0;
+}
+   
