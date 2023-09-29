1108b26ec3c24f194ee46ef5ac446bafae8fe3d1
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2023-09-29 14:52:03 +0530

    Fixed bugs

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -9,11 +9,14 @@ def save_text():
     text = text_entry.get("1.0", "end-1c")
     if text:
         if current_file:
-            file_name = current_file + ".docx"  # Fixed format is .docx
-            doc = docx.Document()
-            doc.add_paragraph(text)
-            doc.save(file_name)
-            messagebox.showinfo("Info", f"Saved to {file_name}")
+            if current_file.endswith(".txt"):
+                with open(current_file, "w") as file:
+                    file.write(text)
+            elif current_file.endswith(".docx"):
+                doc = docx.Document()
+                doc.add_paragraph(text)
+                doc.save(current_file)
+            messagebox.showinfo("Info", f"Saved to {current_file}")
         else:
             save_as()
 
@@ -21,7 +24,8 @@ def open_file():
     global current_file
     file_name = filedialog.askopenfilename(defaultextension=".docx", filetypes=[("Text Documents", "*.txt"), ("Word Documents", "*.docx")])
     if file_name:
-        current_file, file_extension = os.path.splitext(file_name)
+        current_file = file_name
+        file_extension = os.path.splitext(file_name)[1]
         if file_extension == ".txt":
             with open(file_name, "r") as file:
                 text = file.read()
@@ -33,9 +37,9 @@ def open_file():
 
 def save_as():
     global current_file
-    file_name = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Word Documents", "*.docx")])
+    file_name = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Documents", "*.txt"), ("Word Documents", "*.docx")])
     if file_name:
-        current_file, _ = os.path.splitext(file_name)
+        current_file = file_name
         save_text()
 
 # Create the main window
@@ -43,18 +47,18 @@ root = tk.Tk()
 root.title("Text Editor")
 root.geometry("700x900")
 
-# Create buttons for Open, Save, and Save As at the top
+# Create buttons for Open, Save, and Save As
 open_button = tk.Button(root, text="Open", command=open_file)
 save_button = tk.Button(root, text="Save", command=save_text)
 save_as_button = tk.Button(root, text="Save As", command=save_as)
 
-# Grid configuration for buttons at the top
+# Create a text entry widget
+text_entry = tk.Text(root, wrap=tk.WORD, width=80, height=30)
+
+# Grid configuration for buttons and text entry
 open_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 save_button.grid(row=0, column=1, padx=10, pady=10, sticky="w")
 save_as_button.grid(row=0, column=2, padx=10, pady=10, sticky="w")
-
-# Create a text entry widget below the buttons
-text_entry = tk.Text(root, wrap=tk.WORD, width=80, height=30)
 text_entry.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
 
 # Initialize the current file variable
